a1571fdd574c8a1512e4f38c2ba04a1470cf4599
InnovateDrew <InnovateDrew@DESKTOP-PVRDE7T>
2020-06-19 00:21:39 -0700

    Starting to set up main controller

---
+++ Controllers/AnimeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AmmiServices.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnimeController : ControllerBase
+    {
+        private readonly ILogger<AnimeController> _logger;
+        public AnimeController(ILogger<AnimeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+            var rng = new Random();
+            var yo = "gdfgdf";
+        }
+    }
+}
---
+++ Models/Anime.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AmmiServices
+{
+    public class Anime 
+    {
+        public string test1 { get; set; }
+
+        public string test2 { get; set; }
+    }
+}
