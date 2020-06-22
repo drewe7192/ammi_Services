a814b1bfc325af9536dc2a6abbb20b8191a4e579
InnovateDrew <InnovateDrew@DESKTOP-PVRDE7T>
2020-06-21 22:29:01 -0700

    started setting up MongoDB

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmmiServices.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+
+        [BsonElement("Name")]
+        public string firstName { get; set; }
+
+        public string lasttName { get; set; }
+    }
+}
---
+++ Services/UsersService.cs
@@ -0,0 +1,29 @@
+﻿using AmmiServices.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmmiServices.Services
+{
+    public class UsersService
+    {
+
+        private readonly IMongoCollection<User> _users;
+
+        public UsersService( IUsersDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _users = database.GetCollection<User>(settings.UsersCollectionName);
+        }
+
+
+        public List<User> Get() => _users.Find(user => true).ToList();
+
+     /*   public User Get(string id) => _users.Find<User>(user => user.Id === id).FirstOrDefault();*/
+
+    }
+}
